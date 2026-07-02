4f843b635572f643e988bff69ceb25a8b8bd7083
BrianSuhit <braisuhit@gmail.com>
2026-07-02 16:50:21 -0300

    fix: delete comments in node 6

--- 06-prompt-engineering/main_json_prompting.py
+++ 06-prompt-engineering/main_json_prompting.py
@@ -5,19 +5,15 @@ from pydantic import BaseModel
 load_dotenv()
 client = genai.Client()
 
-# Paso 1: Definimos la estructura de datos que nuestro software necesita usando Pydantic.
-# El modelo estará obligado a devolver exactamente estas llaves y estos tipos de datos.
 class SentimentAnalysis(BaseModel):
     sentiment: str
     confidence: float
     key_phrases: list[str]
 
-# Paso 2: Creamos las instrucciones y la consulta del usuario
 system_instruction = "You are a data extraction assistant. Analyze the text and return ONLY a valid JSON."
 user_query = "Review: The battery life is decent, but the screen scratches easily."
 
-# Paso 3: Ejecutamos la inferencia forzando la salida estructurada.
-# Pasamos nuestro esquema de Pydantic directamente en la configuración de la API.
+
 interaction = client.interactions.create(
     model="gemini-3.5-flash",
     system_instruction=system_instruction,
@@ -25,7 +21,7 @@ interaction = client.interactions.create(
     response_format={
         "type": "text",
         "mime_type": "application/json",
-        "schema": SentimentAnalysis.model_json_schema()  # <- Convierte Pydantic a JSON Schema crudo
+        "schema": SentimentAnalysis.model_json_schema()
     }
 )
 valid_result = SentimentAnalysis.model_validate_json(interaction.output_text)
